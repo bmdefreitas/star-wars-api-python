d519a1791405af45c15cff9660a44ef61aff7248
Bruno <bmdefreitas@gmail.com>
2020-03-19 23:08:39 -0300

    Service test unit

---
+++ tests/unit/services.py
@@ -0,0 +1,10 @@
+# import pytest
+
+class ServicesTestClass:
+    def planetas(self):
+        x = "this"
+        assert "h" in x
+
+    def swapi(self):
+        x = "hello"
+        assert "h" in x
